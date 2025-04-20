a0752cc345cc42be46c337f39e26f9103cb850fc
brianbok <brianbok97@gmail.com>
2025-04-21 01:17:56 +0900

    Refact: import 모듈명 수정 및 실제 테이블 연결
    실제 테이블 read작업 및 연결

--- dags/MLops.py
+++ dags/MLops.py
@@ -1,13 +1,13 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-from common.repository.repository import postgreSQL
+from common.filter.repository import postgreSQL
 import pendulum
 
 # target to read/ write table
 
 # save_to_db = postgreSQL("seoulmoa","datawarehouse","SubwayPredict") # MLops result
-read_from_db = postgreSQL("seoulmoa","datawarehouse","Event") # test with event table
+read_from_db = postgreSQL("seoulmoa","datawarehouse","MontlySubwaystation") # test with event table
 
 with DAG (
     dag_id='MLops_pipline',
