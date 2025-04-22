9cf874b8f3c0c86c3c4b0545c276fa6fb47e8be1
복준수 <brianbok97@gmail.com>
2025-04-22 18:07:46 +0900

    Feat: 최초1회 시행 DAG 작성
    MLops 를 위한 초기 모델 구축에 필요한 데이터셋을 수집합니다.
    각각 작년 월별 시간별 데이터, 지난달 일자별 사용량데이터

---
+++ dags/MLops_init_lastmonth.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.models import Variable
+
+from common.base.init.get_subway_initial_data_set import get_data
+from common.jobs.transfer import subwaystation_daily_data
+from common.jobs.repository import postgreSQL
+import pendulum
+
+# batch 처리 api key
+api_key = Variable.get("seoul_api_key")
+# 데이터베이스, 스키마, 테이블명 정의
+save_to_db = postgreSQL("seoulmoa","datawarehouse","subway_data_prev_month")
+
+with DAG (
+    dag_id='MLops_init_get_lastmonth',
+    description="(최초1회!! 이후 unpause 바랍니다.)초기 예측 모델 데이터셋 구축을 위한 DAG입니다. 추후에는 일일데이터 수집으로 대체되며 최초 1회만 실행합니다.",
+    schedule='0 0 7 * *',
+    start_date=pendulum.datetime(2025,4,17, tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+    
+    get_data_=PythonOperator(
+        task_id='get_data',
+        python_callable=get_data, # 전달 데이터를 모두 가져오는 함수
+        op_args=[api_key]
+    )
+
+    refine_data_=PythonOperator(
+        task_id='refine_data',
+        python_callable=subwaystation_daily_data # 일일 데이터 수집 컬럼명과 동일
+    )
+
+    save_data_=PythonOperator(
+        task_id='save_data',
+        python_callable=save_to_db.save_to_subwayDaily_table
+    )
+
+    get_data_>> refine_data_ >>save_data_
