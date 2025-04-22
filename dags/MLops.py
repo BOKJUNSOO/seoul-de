6f74f8dc4ee9aab6c5959fe4f33596d11499fae2
복준수 <brianbok97@gmail.com>
2025-04-22 18:08:52 +0900

    Feat: MLops dag 추가!
    init이 포함된 dag를 실행한후 이를 바탕으로 자동으로 모델을 학습하고
    갱신되는 데이터를 통해 일일 지하철 시간대별 사용량을 예측합니다.

--- dags/MLops.py
+++ dags/MLops.py
@@ -2,12 +2,19 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 from common.jobs.repository import postgreSQL
+from common.model.daily.preprocessing import modeling
+from common.model.daily.postprocessing import predict_all_for_date
+from common.model.daily.calculate import make_hourly_dataframe
+from common.jobs.transfer import subwaystation_prediction_hourly_data
 import pendulum
 
 # target to read/ write table
 
-# save_to_db = postgreSQL("seoulmoa","datawarehouse","SubwayPredict") # MLops result
-read_from_db = postgreSQL("seoulmoa","datawarehouse","MontlySubwaystation") # test with event table
+read_from_db_1 = postgreSQL("seoulmoa","datawarehouse","subway_data_prev_month")
+read_from_db_2 = postgreSQL("seoulmoa","datawarehouse","subway_data_daily")
+read_from_db_3 = postgreSQL("seoulmoa","datawarehouse","subway_data_prev_year")
+
+save_to_db = postgreSQL("seoulmoa","datawarehouse","subway_predict")
 
 with DAG (
     dag_id='MLops_pipline',
@@ -18,9 +25,54 @@ with DAG (
 ) as dag:
 
     #[connect with db]
-    read_data_= PythonOperator(
-        task_id = "read_data",
-        python_callable=read_from_db.read_table
+    read_montly_data_=PythonOperator(
+        task_id="read_montly_data",
+        python_callable=read_from_db_1.read_table
     )
 
-    read_data_
+    read_daily_data_=PythonOperator(
+        task_id="read_daily_data",
+        python_callable=read_from_db_2.read_table
+    )
+
+    #[preprocessed and train daily model]
+    train_daily_model=PythonOperator(
+        task_id='train_daily_model',
+        python_callable=modeling
+    )
+
+    #[postprocessed and make daily prediction]
+    make_daily_prediction=PythonOperator(
+        task_id='make_daily_prediction',
+        python_callable=predict_all_for_date
+    )
+
+    read_year_data_=PythonOperator(
+        task_id='read_year_day',
+        python_callable=read_from_db_3.read_table
+    )
+
+    calculate_ratio_=PythonOperator(
+        task_id='calculate_ratio',
+        python_callable=make_hourly_dataframe
+    )
+
+    refine_data_=PythonOperator(
+        task_id='refine_data',
+        python_callable=subwaystation_prediction_hourly_data
+    )
+
+    save_data_=PythonOperator(
+        task_id='save_data',
+        python_callable=save_to_db.save_to_hourly_predict
+    )
+
+
+    (
+    [read_montly_data_, read_daily_data_] 
+    >> train_daily_model 
+    >>[make_daily_prediction ,read_year_data_] 
+    >> calculate_ratio_
+    >> refine_data_
+    >> save_data_
+    )
