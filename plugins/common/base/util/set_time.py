0aee400a82bdc39df0bb30e78dee18b7e44eda3d
복준수 <brianbok97@gmail.com>
2025-04-22 18:10:13 +0900

    Feat: 시간 설정을 위한 set_time 함수 추가
    지난달, 작년의 시간대 조정을 위한 함수 추가
    init 조건에서 사용됨

--- plugins/common/base/util/set_time.py
+++ plugins/common/base/util/set_time.py
@@ -12,3 +12,12 @@ def get_month_dates(year:int, month:int) -> list:
     
     # 날짜 리스트 생성
     return [datetime.date(year, month, day).strftime("%Y%m%d") for day in range(1, last_day + 1)]
+
+def get_last_year_months(current_year) -> list:
+    """
+    시작 연도를 입력하면
+
+    %Y%m형태로 작년 연/월을 리턴하는 함수
+    """
+    last_year = current_year - 1
+    return [last_year * 100 + month for month in range(1, 13)]
