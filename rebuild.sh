5b43b54c8833784f29015d4ad27b31c9d9273803
MingyooLee <hoobastank0807@gmail.com>
2025-04-27 03:20:16 +0900

    config 체크 로직 추가

--- rebuild.sh
+++ rebuild.sh
@@ -6,6 +6,14 @@ cd "$(dirname "$0")"
 echo "빌드 시작"
 
 docker compose down
+
+echo "airflow.cfg 상태 체크 중..."
+
+if [ -d ./config/airflow.cfg ]; then
+    echo "airflow.cfg가 디렉토리입니다. 삭제합니다."
+    rm -rf ./config/airflow.cfg
+fi
+
 docker compose up --build -d
 
 echo "빌드 완료!"
