a9ad3f3e33fb1a84634e953ab0fd4d3042bae155
복준수 <brianbok97@gmail.com>
2025-04-29 00:59:04 +0900

    Feat: 금일 task가 event 테이블 수집인지 그렇지 않은지를 확인하는 함수.

--- plugins/common/base/util/check_task.py
+++ plugins/common/base/util/check_task.py
@@ -3,3 +3,13 @@ def check_status(**kwargs):
     ti = kwargs['ti']
     status = str(ti.xcom_pull(task_ids='check_data_',key='key'))
     return status
+
+# check_daily_의 값을 결정하는 함수
+def check_daily(**kwargs):
+    ti = kwargs['ti']
+    status = str(ti.xcom_pull(task_ids='check_data_',key='key'))
+    if status == "save_to_event_":
+        status = "refine_data_"
+    else:
+        status = "refine_data_s"
+    return status
